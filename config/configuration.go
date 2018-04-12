c49d869a6804ad85b8915a2895b24e64ec456c8a
Simon <simonpelczer@outlook.de>
2018-04-12 22:00:39 +0200

    :construction: Consumer is receiving on defined topics
    Pushing current state of the connector. Currently the makeConsumer function takes in the configuration and setups and consumer accordingly to it.
    
    For now this consumer is able to receive messages on the defined topics.
    
    Added missing field to the configuration and also made the defaults more meaningful.
    
    Temporary renamed the docker.yml to docker-compose
    
    Signed-off by: Simon Pelczer <templum.dev@gmail.com>

--- config/configuration.go
+++ config/configuration.go
@@ -15,17 +15,31 @@ const rabbitHost = "rabbit_mq_host"
 const rabbitPort = "rabbit_mq_port"
 const rabbitUser = "rabbit_mq_user"
 const rabbitPass = "rabbit_mq_pass"
+const rabbitExchange = "rabbit_mq_exchange"
+const rabbitQueue = "rabbit_mq_queue"
 
-type connectorConfig struct {
+type ConnectorConfig struct {
 	GatewayURL            string
 	Topics                []string
 	RabbitMQConnectionURI string
+	ExchangeName          string
+	QueueName             string
 }
 
-func BuildConnectorConfig() connectorConfig {
+func BuildConnectorConfig() ConnectorConfig {
 	gatewayURL := "http://gateway:8080"
 	if val, exists := os.LookupEnv(gatewayUrl); exists {
 		gatewayURL = val
+	}
+
+	exchangeName := "OpenFaaS-Exchange"
+	if val, exists := os.LookupEnv(rabbitExchange); exists {
+		exchangeName = val
+	}
+
+	queueName := "FaaS Worker Queue"
+	if val, exists := os.LookupEnv(rabbitQueue); exists {
+		queueName = val
 	}
 
 	topics := []string{}
@@ -43,17 +57,19 @@ func BuildConnectorConfig() connectorConfig {
 
 	rabbitURI := generateRabbitConnectionUri()
 
-	return connectorConfig{
+	return ConnectorConfig{
 		GatewayURL:            gatewayURL,
 		Topics:                topics,
 		RabbitMQConnectionURI: rabbitURI,
+		ExchangeName:          exchangeName,
+		QueueName:             queueName,
 	}
 }
 
 // Generates the connection string to RabbitMQ based on
 // information provided through environment variables
 func generateRabbitConnectionUri() string {
-	host := "rabbitqueue"
+	host := "localhost"
 	if val, exists := os.LookupEnv(rabbitHost); exists {
 		host = val
 	}
@@ -68,7 +84,7 @@ func generateRabbitConnectionUri() string {
 		port = val
 	}
 
-	pass := "user"
+	pass := "pass"
 	if val, exists := os.LookupEnv(rabbitPass); exists {
 		port = val
 	}
--- main.go
+++ main.go
@@ -6,31 +6,29 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/streadway/amqp"
 	"github.com/Templum/rabbitmq-connector/config"
-	"os"
+	"github.com/streadway/amqp"
+	"time"
 )
 
-func failOnError(err error, msg string){
+func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
 		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
 
-func receiveMessagesFromTopic(){
-	conf := config.BuildConnectorConfig()
+func makeConsumer(conf config.ConnectorConfig) {
 	con, err := amqp.Dial(conf.RabbitMQConnectionURI)
-	failOnError(err, "Failed to open a channel")
+	failOnError(err, "Failed to create a connection")
 	defer con.Close()
 
 	ch, err := con.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	//TODO: Build up connection pool for provided topics
 	err = ch.ExchangeDeclare(
-		"OpenFaaS-Topic",
+		conf.ExchangeName,
 		"topic",
 		true,
 		false,
@@ -41,87 +39,95 @@ func receiveMessagesFromTopic(){
 	failOnError(err, "Failed to declare an exchange")
 
 	queue, err := ch.QueueDeclare(
-		"OpenFaaS-Queue",    // name
-		false, // durable
-		false, // delete when usused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
+		conf.QueueName,
+		false,
+		false,
+		true,
+		false,
+		nil,
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	for _, s := range conf.Topics{
-		log.Printf("Binding queue %s to exchange %s with routing key %s",
-			queue.Name, "logs_topic", s)
+	for _, topic := range conf.Topics {
+		log.Printf("Binding queue %s to exchange %s with Topic: %s", conf.QueueName, conf.ExchangeName, topic)
 		err = ch.QueueBind(
-			queue.Name,       // queue name
-			s,            // routing key
-			"OpenFaaS-Topic", // exchange
+			conf.QueueName,
+			topic,
+			conf.ExchangeName,
 			false,
-			nil)
-		failOnError(err, "Failed to bind a queue")
+			nil,
+		)
+		failOnError(err, fmt.Sprintf("Failed to bind %s", conf.QueueName))
 	}
 
-	msgs, err := ch.Consume(
-		queue.Name, // queue
-		"FaaS Worker",     // consumer
-		true,   // auto ack
-		false,  // exclusive
-		false,  // no local
-		false,  // no wait
-		nil,    // args
+	messages, err := ch.Consume(
+		queue.Name,
+		"OpenFaaS Consumer",
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
-	failOnError(err, "Failed to register a consumer")
+	failOnError(err, "Failed to register OpenFaaS Consumer")
 
 	forever := make(chan bool)
 
 	go func() {
-		for d := range msgs {
-			log.Printf(" [x] %s", d.Body)
+		for msg := range messages {
+			log.Printf("Received Message [%s] on Topic [%s] of Type [%s]", msg.Body, msg.RoutingKey, msg.ContentType)
 		}
 	}()
 
-	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
 	<-forever
 }
 
-func emitMessagesOnTopic(){
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+func emitMessagesOnTopic() {
+	conf := config.BuildConnectorConfig()
+	con, err := amqp.Dial(conf.RabbitMQConnectionURI)
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	defer con.Close()
 
-	ch, err := conn.Channel()
+	ch, err := con.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"logs_topic", // name
-		"topic",      // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
+		"OpenFaaS-Topic",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
 	failOnError(err, "Failed to declare an exchange")
+	for {
 
-	err = ch.Publish(
-		"logs_topic",          // exchange
-		"", // routing key
-		false, // mandatory
-		false, // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	failOnError(err, "Failed to publish a message")
+		body := "Awesome Message"
 
-	log.Printf(" [x] Sent %s", body)
+		err = ch.Publish(
+			conf.ExchangeName, // exchange
+			"not",             // routing key
+			false,             // mandatory
+			false,             // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+
+		log.Printf(" [x] Sent %s", body)
+
+		time.Sleep(2 * time.Second)
+	}
 }
 
-
-
-
 func main() {
-	receiveMessagesFromTopic()
+	conf := config.BuildConnectorConfig()
+	go makeConsumer(conf)
+
+	go emitMessagesOnTopic()
+	forever := make(chan bool)
+	<-forever
 }
