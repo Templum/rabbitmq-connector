58a96af9253683c93cb36cc41b0358b7723dfff2
Templum <templum.dev@gmail.com>
2018-05-04 21:32:30 +0200

    :sparkles: Added Util for HTTP Client
    Signed-off by: Simon Pelczer <templum.dev@gmail.com>

---
+++ sdk/make_client.go
@@ -0,0 +1,22 @@
+package sdk
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+func MakeClient(timeout time.Duration) *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   timeout,
+				KeepAlive: 10 * time.Second,
+			}).DialContext,
+			MaxIdleConns:        100,
+			MaxIdleConnsPerHost: 100,
+			IdleConnTimeout:     120 * time.Millisecond,
+		},
+	}
+}
