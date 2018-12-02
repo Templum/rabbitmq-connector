37d00cde112d4800f036e5ee8b4b0ca47495c418
Simon <simonpelczer@outlook.de>
2018-12-02 14:26:53 +0100

    :sparkles: Using local Creds when in DEV Mode. Listening for SIGINT
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>

--- main.go
+++ main.go
@@ -5,21 +5,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Templum/rabbitmq-connector/pkg/config"
-	"github.com/openfaas/faas-provider/auth"
-
 	"github.com/Templum/rabbitmq-connector/pkg/rabbitmq"
 	"github.com/Templum/rabbitmq-connector/pkg/version"
 	"github.com/openfaas-incubator/connector-sdk/types"
+	"github.com/openfaas/faas-provider/auth"
 )
 
 func main() {
 	commit, version := version.GetReleaseInfo()
 	log.Printf("OpenFaaS RabbitMQ Connector [Version: %s Commit: %s]", version, commit)
 
-	creds := &auth.BasicAuthCredentials{User: "admin", Password: "b43c1de00d8a477d6af007a6516944e3d1b02692a190fe71f68616b678ac959a"} // Local
-	// creds := types.GetCredentials()
+	creds := types.GetCredentials()
+	if version == "dev" {
+		creds = &auth.BasicAuthCredentials{User: "admin", Password: "b43c1de00d8a477d6af007a6516944e3d1b02692a190fe71f68616b678ac959a"} // Local
+	}
 
 	controllerConf := &types.ControllerConfig{
 		RebuildInterval: config.GetRefreshTime(),
@@ -37,9 +41,18 @@ func main() {
 	connector.StartConnector()
 	defer connector.Close()
 
-	// TODO: Listen for Signals
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
 
-	forever := make(chan bool)
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+
+	sig := <-signalChannel
+	switch sig {
+	case os.Interrupt:
+		log.Printf("Recieved SIGINT preparing for shutdown")
+		connector.Close()
+	case syscall.SIGTERM:
+		log.Printf("Recieved SIGTERM shutting down")
+		connector.Close()
+	}
 }
