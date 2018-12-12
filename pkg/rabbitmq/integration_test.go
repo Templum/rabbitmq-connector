8c11b90f7f329ed7e338c12c2f6ca0aff43508e5
Simon <simonpelczer@outlook.de>
2018-12-12 21:10:41 +0100

    :white_check_mark: Created Initial Setup for Integration Test
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>

---
+++ pkg/rabbitmq/integration_test.go
@@ -0,0 +1,84 @@
+package rabbitmq
+
+import (
+	"flag"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	integration = flag.Bool("integration", false, "run integration test")
+)
+
+
+
+const EnvMQTopics = "RMQ_TOPICS"
+const TOPICS = "account,support"
+const TEST_URI = "amqp://user:pass@localhost:5672/"
+
+// Mocks
+type MockOpenFaaS struct {
+	finishChannel chan bool
+}
+
+func (m *MockOpenFaaS) Invoke(topic string, message *[]byte) {
+
+}
+
+func CreateMock() Invoker{
+	return & MockOpenFaaS{
+		nil,
+	}
+}
+
+
+func TestMakeConnector(t *testing.T) {
+	flag.Parse()
+	if !*integration {
+		t.Skip()
+	}
+
+	setupEnvironment(t)
+
+
+	t.Run("Initialize without issues", func(t *testing.T) {
+		target := MakeConnector(TEST_URI, CreateMock())
+		target.Start()
+		time.Sleep(5 * time.Second)
+		teardownTarget(t, target)
+	})
+
+	t.Run("Should quick Stop", func(t *testing.T) {
+		target := MakeConnector(TEST_URI, CreateMock())
+		target.Close()
+		time.Sleep(5 * time.Second)
+		teardownTarget(t, target)
+	})
+
+	t.Run("Should full Stop", func(t *testing.T) {
+		target := MakeConnector(TEST_URI, CreateMock())
+		target.Start()
+		time.Sleep(5 * time.Second)
+		target.Close()
+		time.Sleep(5 * time.Second)
+		teardownTarget(t, target)
+	})
+
+	teardown(t)
+}
+
+func teardownTarget(t *testing.T, target Connector)  {
+	t.Helper()
+	target.Close()
+}
+
+func setupEnvironment(t *testing.T) {
+	t.Helper()
+	os.Setenv(EnvMQTopics, TOPICS)
+}
+
+func teardown(t *testing.T){
+	t.Helper()
+	os.Unsetenv(EnvMQTopics)
+}
