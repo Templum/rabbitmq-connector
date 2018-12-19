328120caff0075e70a5568d2d92bbca953f3bc0d
Simon <simonpelczer@outlook.de>
2018-12-19 21:32:57 +0100

    :white_check_mark: Extended Unit Testing (#13)
    * :recycle: Renamed StartConnector => Start
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :recycle: Refactored Connector to improve testing
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :white_check_mark: Created Tests for Connector
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :bug: Fixed Data Race in Test related coding
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :art: Formated Code
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :heavy_plus_sign: :fire: Added Docker Name Generator + Pruning No Go
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :art: Minor
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :recycle: :sparkles: Refactored for better Testing & Added Naming Worker
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :recycle: Created Interface for OpenFaaS Invoker
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :wrench: Removed unsupported field
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :recycle: Refactored handleError and using Invoker Interface
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :construction_worker: Added Docker Setup to Travis
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :white_check_mark: Created Initial Setup for Integration Test
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :construction_worker: Enabled Integration Test during CI
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :construction_worker: Enabled Integration Test during CI
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>
    
    * :white_check_mark: Added test for failing connection establishing
    
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>

--- pkg/rabbitmq/connector.go
+++ pkg/rabbitmq/connector.go
@@ -6,28 +6,26 @@ import (
 	"log"
 	"math"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/Templum/rabbitmq-connector/pkg/config"
-	"github.com/openfaas-incubator/connector-sdk/types"
 	"github.com/streadway/amqp"
 )
+
 
 type connector struct {
 	uri    string
 	closed bool
 
 	con    *amqp.Connection
-	client *types.Controller
+	client Invoker
 
 	// Consumers
 	workers []*worker
-
-	// Sig Channel
-	errorChannel chan *amqp.Error
 }
 
-func MakeConnector(uri string, client *types.Controller) *connector {
+func MakeConnector(uri string, client Invoker) Connector {
 	return &connector{
 		uri,
 		false,
@@ -36,91 +34,141 @@ func MakeConnector(uri string, client *types.Controller) *connector {
 		client,
 
 		nil,
-		nil,
 	}
 }
 
-func (c *connector) StartConnector() {
-	log.Println("Starting Connector")
-
-	c.init()
+// Invoker is the Interface used by the OpenFaaS Connector SDK to perform invocations
+// of Lambdas based on a provided topic and message
+type Invoker interface {
+	Invoke(topic string, message *[]byte)
 }
 
-func (c *connector) Close() {
-	if !c.closed {
-		log.Println("Shutting down Connector")
-		c.closed = true
-		defer c.con.Close()
-
-		for _, worker := range c.workers {
-			worker.Close()
-		}
-		c.workers = nil
-	}
+type Recoverer interface {
+	recover(receivedError *amqp.Error)
 }
 
-func (c *connector) init() {
+type Connector interface {
+	Start()
+	Close()
+	Recoverer
+}
+
+func (c *connector) Start() {
+	log.Println("Starting Init Process of Connector")
 	var err error
 	c.con, err = connectToRabbitMQ(c.uri, 3)
 	if err != nil {
-		log.Panicf("Failed to connect to %s, recieved %s", c.uri, err)
-	} else {
-		log.Printf("Successfully connected to %s", c.uri)
+		log.Panicf("Failed to connect to %s after 3 retries", SanitizeConnectionUri(c.uri))
 	}
+
+	log.Printf("Successfully established connection with %s", SanitizeConnectionUri(c.uri))
 
 	// Related to Self Healing
-	c.errorChannel = make(chan *amqp.Error)
-	c.con.NotifyClose(c.errorChannel)
-	go c.registerSelfHealer() // Leaking Go Routine ?
+	oneTimeErrorChannel := make(chan *amqp.Error) // Maybe switch later to an struct holding the channel
+	c.con.NotifyClose(oneTimeErrorChannel)
+	go Healer(c, oneTimeErrorChannel)
 
-	// Queues: 1 Topic === 1 Queue
-	topics := config.GetTopics()
+	c.spawnWorkers(config.GetTopics())
+
+	log.Println("Connector finished Init Process and is now running")
+}
+
+func (c *connector) recover(receivedError *amqp.Error) {
+	if c.closed {
+		return
+	}
+
+	if receivedError.Recover {
+		log.Printf("Performing a recovery from following recoverable error: [Status: %d Reason: %s]", receivedError.Code, receivedError.Reason)
+	} else {
+		log.Panicf("Recieved unrecovarable error: [Status: %d Reason: %s]", receivedError.Code, receivedError.Reason)
+	}
+
+	log.Println("Will now clear worker pool")
+	c.clearWorkers()
+
+	c.Start()
+}
+
+func (c *connector) Close() {
+	if !c.closed && c.con != nil {
+		log.Println("Shutting down Connector")
+		c.closed = true
+		defer c.con.Close()
+		log.Println("Clearing worker pool")
+		c.clearWorkers()
+	}
+}
+
+// spawnWorkers will spawn Workers per topic based on the available resources. Further it
+// assigns the newly created worker to the worker pool before starting them.
+func (c *connector) spawnWorkers(topics []string) {
 	amountOfTopics := len(topics)
-	for _, topic := range topics {
-		workerCount := int(math.Round(float64(runtime.NumCPU()*2)/float64(amountOfTopics))) + 1
-		log.Printf("Spawning %d Workers for Topic: %s", workerCount, topic)
+	workerCount := CalculateWorkerCount(amountOfTopics)
+	log.Printf("%d Topics are registered. Will be spawning %d Workers per Topic. ", amountOfTopics, workerCount)
 
+	for _, topic := range topics {
 		for i := 0; i < workerCount; i++ {
 			worker := NewWorker(c.con, c.client, topic)
-			worker.Start()
-			// TODO: Maybe add Thread Lock here
 			c.workers = append(c.workers, worker)
-		}
-	}
-
-}
-
-func (c *connector) registerSelfHealer() {
-	for {
-		err := <-c.errorChannel
-		if !c.closed {
-			log.Printf("Recieved following error %s", err)
-
-			time.Sleep(30 * time.Second)
-			c.recover()
+			go worker.Start()
 		}
 	}
 }
 
-func (c *connector) recover() {
-	log.Printf("Performing a Recovery")
-
-	for _, topicQueue := range c.workers {
-		topicQueue.Close()
+// clearWorkers will stop Workers from the worker pool and afterwards cleanup the references
+func (c *connector) clearWorkers() {
+	for _, worker := range c.workers {
+		worker.Close()
 	}
 	c.workers = nil
-
-	c.init()
 }
 
+// Helper Functions
+
+// TODO: Documentation & Eventuell Type für Connection (AMK)
 func connectToRabbitMQ(uri string, retries int) (*amqp.Connection, error) {
 	con, err := amqp.Dial(uri)
 
 	if err != nil && retries > 0 {
-		log.Printf("Failed to connect to %s with error %s. Retries left %d", uri, err, retries)
+		log.Printf("Failed to connect to %s with error %s. Retries left %d", SanitizeConnectionUri(uri), err, retries)
 		time.Sleep(5 * time.Second)
 		return connectToRabbitMQ(uri, retries-1)
 	}
 
 	return con, err
+}
+
+// Healer will take the reference to something "Recoverable"  along with an error channel for amqp errors.
+// The method is only meant for one time usage.
+func Healer(c Recoverer, errorStream chan *amqp.Error) {
+	err := <-errorStream
+	// Give it some time before beginning recovering
+	time.Sleep(30 * time.Second)
+	c.recover(err)
+}
+
+// CalculateWorkerCount will calculate the amount of workers that will be spawned
+// based on the following formula: Available CPU's * 2 / Amount of Topics
+func CalculateWorkerCount(amountOfTopics int) int {
+	targetGoRoutines := float64(runtime.NumCPU() * 2)
+
+	if int(targetGoRoutines) < amountOfTopics {
+		return int(math.Floor(targetGoRoutines)/float64(amountOfTopics)) + 1
+	} else {
+		return int(math.Floor(targetGoRoutines) / float64(amountOfTopics))
+	}
+
+}
+
+// SanitizeConnectionUri takes an uri in the format user:pass@domain and removes the sensitive credentials.
+// Prior it will perform a check for @, if it is not included it will return the unchanged uri
+func SanitizeConnectionUri(uri string) string {
+	const SEPARATOR = "@"
+
+	if idx := strings.Index(uri, SEPARATOR); idx != -1 {
+		return strings.Split(uri, SEPARATOR)[1]
+	} else {
+		return uri
+	}
 }
---
+++ pkg/rabbitmq/connector_test.go
@@ -0,0 +1,107 @@
+package rabbitmq
+
+import (
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+// Mocks
+type MockRecoverer struct {
+	finishChannel chan bool
+}
+
+func CreateRecoverer(finished chan bool) *MockRecoverer {
+	return &MockRecoverer{finished}
+}
+
+func (m *MockRecoverer) recover(receivedError *amqp.Error) {
+	m.finishChannel <- true
+	close(m.finishChannel)
+}
+
+// Tests
+
+func TestSanitizeConnectionUri(t *testing.T) {
+	t.Run("Sanitize Credentials", func(t *testing.T) {
+		const CONTAINS_CREDS = "amqp://user:pass@localhost:5672/"
+		sanitized := SanitizeConnectionUri(CONTAINS_CREDS)
+
+		if idx := strings.Index(sanitized, "user:pass"); idx != -1 {
+			t.Errorf("Expected %s recieved %s", "localhost:5672/", sanitized)
+		}
+	})
+
+	t.Run("Not Modify", func(t *testing.T) {
+		const CONTAINS_NO_CREDS = "amqp://localhost:5672"
+		sanitized := SanitizeConnectionUri(CONTAINS_NO_CREDS)
+
+		if sanitized != CONTAINS_NO_CREDS {
+			t.Errorf("Expected %s recieved %s", CONTAINS_NO_CREDS, sanitized)
+		}
+	})
+}
+
+func TestCalculateWorkerCount(t *testing.T) {
+	t.Run("All Workers on one topic", func(t *testing.T) {
+		target := runtime.NumCPU() * 2
+
+		calculated := CalculateWorkerCount(1)
+
+		if calculated != target {
+			t.Errorf("Expected %d recieved %d", target, calculated)
+		}
+	})
+
+	t.Run("Should Split between two topics", func(t *testing.T) {
+		target := runtime.NumCPU()
+
+		calculated := CalculateWorkerCount(2)
+
+		if calculated != target {
+			t.Errorf("Expected %d recieved %d", target, calculated)
+		}
+	})
+
+	t.Run("Exactly one worker per topic", func(t *testing.T) {
+		target := 1
+
+		calculated := CalculateWorkerCount(runtime.NumCPU() * 2)
+
+		if calculated != target {
+			t.Errorf("Expected %d recieved %d", target, calculated)
+		}
+	})
+
+	t.Run("At least one worker", func(t *testing.T) {
+		target := 1
+
+		calculated := CalculateWorkerCount(runtime.NumCPU()*2 + 2)
+
+		if calculated != target {
+			t.Errorf("Expected %d recieved %d", target, calculated)
+		}
+	})
+}
+
+func TestHealer(t *testing.T) {
+	mockStream := make(chan *amqp.Error)
+	calledStream := make(chan bool)
+
+	go Healer(CreateRecoverer(calledStream), mockStream)
+
+	// Simulate Error
+	mockStream <- &amqp.Error{Recover: false, Reason: "Expected Error", Server: false}
+	close(mockStream)
+
+	select {
+	case called := <-calledStream:
+		if called {
+		}
+	case <-time.After(31 * time.Second):
+		t.Errorf("Healer did not call recover() after %d Seconds", 31)
+	}
+}
