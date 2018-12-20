6cef5dcd6d0ee75e12983930802fa444ecad54bc
Simon <simonpelczer@outlook.de>
2018-12-20 20:17:50 +0100

    :art: Formated Project
    Signed-off-by: Simon Pelczer <templum.dev@gmail.com>

--- pkg/rabbitmq/integration_test.go
+++ pkg/rabbitmq/integration_test.go
@@ -11,8 +11,6 @@ var (
 	integration = flag.Bool("integration", false, "run integration test")
 )
 
-
-
 const EnvMQTopics = "RMQ_TOPICS"
 const TOPICS = "account,support"
 const TEST_URI = "amqp://user:pass@localhost:5672/"
@@ -26,12 +24,11 @@ func (m *MockOpenFaaS) Invoke(topic string, message *[]byte) {
 
 }
 
-func CreateMock() Invoker{
-	return & MockOpenFaaS{
+func CreateMock() Invoker {
+	return &MockOpenFaaS{
 		nil,
 	}
 }
-
 
 func TestMakeConnector(t *testing.T) {
 	flag.Parse()
@@ -79,7 +76,7 @@ func TestMakeConnector(t *testing.T) {
 	teardown(t)
 }
 
-func teardownTarget(t *testing.T, target Connector)  {
+func teardownTarget(t *testing.T, target Connector) {
 	t.Helper()
 	target.Close()
 }
@@ -89,7 +86,7 @@ func setupEnvironment(t *testing.T) {
 	os.Setenv(EnvMQTopics, TOPICS)
 }
 
-func teardown(t *testing.T){
+func teardown(t *testing.T) {
 	t.Helper()
 	os.Unsetenv(EnvMQTopics)
 }
