e3c0ccd021c06a120cb2ab3b0315b23ee92b3696
Simon Pelczer <templum.dev@gmail.com>
2019-11-12 20:20:42 +0100

    :sparkles: Implemented Invoke (Sync/Async)

--- pkg/openfaas/client.go
+++ pkg/openfaas/client.go
@@ -18,8 +18,8 @@ type Client struct {
 	url         string
 }
 
-// Invoke TODO:
-func (c *Client) Invoke(ctx context.Context, name string, payload []byte) ([]byte, error) { // TODO: either reuse provided payload or make it pasable
+// InvokeSync TODO:
+func (c *Client) InvokeSync(ctx context.Context, name string, payload []byte) ([]byte, error) { // TODO: either reuse provided payload or make it pasable
 	functionURL := fmt.Sprintf("%s/function/%s", c.url, name)
 
 	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, functionURL, bytes.NewReader(payload))
@@ -36,21 +36,55 @@ func (c *Client) Invoke(ctx context.Context, name string, payload []byte) ([]byt
 		return nil, errors.Wrapf(err, "unable to invoke function %s", name)
 	}
 
+	var output []byte
+
 	if res.Body != nil {
 		defer res.Body.Close()
-		output, _ := ioutil.ReadAll(res.Body)
-
-		switch res.StatusCode {
-		case 200:
-			return output, nil
-		case 401:
-			return nil, errors.New("OpenFaaS Credentials are invalid")
-		case 404:
-			return nil, errors.New(fmt.Sprintf("Function %s is not deployed", name))
-		default:
-			return nil, errors.New(fmt.Sprintf("Recieved unexpected Status Code %d", res.StatusCode))
-		}
+		output, _ = ioutil.ReadAll(res.Body)
 	}
 
-	return nil, nil
+	switch res.StatusCode {
+	case 200:
+		return output, nil
+	case 401:
+		return nil, errors.New("OpenFaaS Credentials are invalid")
+	case 404:
+		return nil, errors.New(fmt.Sprintf("Function %s is not deployed", name))
+	default:
+		return nil, errors.New(fmt.Sprintf("Received unexpected Status Code %d", res.StatusCode))
+	}
+}
+
+// InvokeAsync TODO:
+func (c *Client) InvokeAsync(ctx context.Context, name string, payload []byte) (bool, error) { // TODO: either reuse provided payload or make it pasable
+	functionURL := fmt.Sprintf("%s/async-function/%s", c.url, name)
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, functionURL, bytes.NewReader(payload))
+	if c.credentials != nil {
+		req.SetBasicAuth(c.credentials.User, c.credentials.Password)
+	}
+
+	if req.Body != nil {
+		defer req.Body.Close()
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return false, errors.Wrapf(err, "unable to invoke function %s", name)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	switch res.StatusCode {
+	case 202:
+		return true, nil
+	case 401:
+		return false, errors.New("OpenFaaS Credentials are invalid")
+	case 404:
+		return false, errors.New(fmt.Sprintf("Function %s is not deployed", name))
+	default:
+		return false, errors.New(fmt.Sprintf("Received unexpected Status Code %d", res.StatusCode))
+	}
 }
