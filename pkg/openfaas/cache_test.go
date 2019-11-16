7b25233e95a68cfaade94e072d8a700d61d70d88
Simon Pelczer <templum.dev@gmail.com>
2019-11-16 15:55:57 +0100

    :white_check_mark: Added Test for cache

---
+++ pkg/openfaas/cache_test.go
@@ -0,0 +1,44 @@
+package openfaas
+
+import "testing"
+
+func TestTopicMap(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Should override cache with update", func(t *testing.T) {
+		cache := NewTopicFunctionCache()
+		update := make(map[string][]string)
+		update["billing"] = []string{"taxes", "notify"}
+
+		before := len(cache.topicMap)
+		cache.Refresh(update)
+		after := len(cache.topicMap)
+
+		if before == after {
+			t.Errorf("Expected cache content to be different after update")
+		}
+	})
+
+	t.Run("Should return all found functions for topic", func(t *testing.T) {
+		cache := NewTopicFunctionCache()
+		update := make(map[string][]string)
+		update["billing"] = []string{"taxes", "notify"}
+		cache.Refresh(update)
+
+		found := cache.GetFunctionsFor("billing")
+
+		if len(found) != 2 {
+			t.Errorf("Expected 2 entries for billing but received %d entries", len(found))
+		}
+	})
+
+	t.Run("Should return empty list if topic does not exist", func(t *testing.T) {
+		cache := NewTopicFunctionCache()
+
+		found := cache.GetFunctionsFor("billing")
+
+		if len(found) > 0 {
+			t.Errorf("Expected empty list but received list with %d entries", len(found))
+		}
+	})
+}
