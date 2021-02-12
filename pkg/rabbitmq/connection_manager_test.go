200956ca6bdc2792e9fd400f0b0906aad0fa4c70
Templum <templum.dev@gmail.com>
2021-02-12 17:23:22 +0100

    :white_check_mark: Created tests for connection manager

--- pkg/rabbitmq/connection_manager_test.go
+++ pkg/rabbitmq/connection_manager_test.go
@@ -8,6 +8,7 @@ package rabbitmq
 import (
 	"crypto/tls"
 	"errors"
+	"sync"
 	"testing"
 
 	"github.com/streadway/amqp"
@@ -15,24 +16,46 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+type conMock struct {
+	mock.Mock
+}
+
+func (c *conMock) NotifyClose(receiver chan *amqp.Error) chan *amqp.Error {
+	args := c.Called(nil)
+	return args.Get(0).(chan *amqp.Error)
+}
+
+func (c *conMock) Close() error {
+	args := c.Called(nil)
+	return args.Error(0)
+}
+
+func (c *conMock) Channel() (*amqp.Channel, error) {
+	args := c.Called(nil)
+	return args.Get(0).(*amqp.Channel), args.Error(1)
+}
+
 type brokerMocker struct {
 	mock.Mock
 }
 
-func (b *brokerMocker) Dial(url string) (*amqp.Connection, error) {
+func (b *brokerMocker) Dial(url string) (RBConnection, error) {
 	args := b.Called(url)
-	return args.Get(0).(*amqp.Connection), args.Error(1)
+	return args.Get(0).(RBConnection), args.Error(1)
 }
 
-func (b *brokerMocker) DialTLS(url string, conf *tls.Config) (*amqp.Connection, error) {
+func (b *brokerMocker) DialTLS(url string, conf *tls.Config) (RBConnection, error) {
 	args := b.Called(url, conf)
-	return args.Get(0).(*amqp.Connection), args.Error(1)
+	return args.Get(0).(RBConnection), args.Error(1)
 }
 
 func TestConnectionManager_Connect(t *testing.T) {
 	t.Run("Should connect to specified Rabbit MQ host and return close channel", func(t *testing.T) {
+		con := new(conMock)
+		con.On("NotifyClose", nil).Return(make(chan *amqp.Error))
+
 		broker := new(brokerMocker)
-		broker.On("Dial", "amqp://user:pass@localhost:5672/").Return(&amqp.Connection{}, nil)
+		broker.On("Dial", "amqp://user:pass@localhost:5672/").Return(con, nil)
 
 		target := NewConnectionManager(broker)
 
@@ -41,11 +64,14 @@ func TestConnectionManager_Connect(t *testing.T) {
 		assert.NoError(t, err, "should not throw")
 		assert.NotNil(t, ch, "should not be nil")
 		broker.AssertExpectations(t)
+		con.AssertExpectations(t)
 	})
 
 	t.Run("Should try at least 3 times to connect to Rabbit MQ before throwing error", func(t *testing.T) {
+		con := new(conMock)
+
 		broker := new(brokerMocker)
-		broker.On("Dial", "amqp://user:pass@localhost:5672/").Return(&amqp.Connection{}, errors.New("expected"))
+		broker.On("Dial", "amqp://user:pass@localhost:5672/").Return(con, errors.New("expected"))
 
 		target := NewConnectionManager(broker)
 
@@ -56,4 +82,51 @@ func TestConnectionManager_Connect(t *testing.T) {
 		broker.AssertExpectations(t)
 		broker.AssertNumberOfCalls(t, "Dial", 3)
 	})
+}
+
+func TestConnectionManager_Disconnect(t *testing.T) {
+	t.Run("Should disconnect from Rabbit MQ and clear connection", func(t *testing.T) {
+		con := new(conMock)
+		con.On("Close", nil).Return(nil)
+
+		manager := ConnectionManager{
+			con:  con,
+			lock: sync.RWMutex{},
+		}
+
+		manager.Disconnect()
+		assert.Nil(t, manager.con, "should be nil")
+		con.AssertExpectations(t)
+	})
+
+	t.Run("Should disconnect from Rabbit MQ and clear connection", func(t *testing.T) {
+		con := new(conMock)
+		con.On("Close", nil).Return(errors.New("some error"))
+
+		manager := ConnectionManager{
+			con:  con,
+			lock: sync.RWMutex{},
+		}
+
+		manager.Disconnect()
+		assert.Nil(t, manager.con, "should be nil")
+		con.AssertExpectations(t)
+	})
+}
+
+func TestConnectionManager_Channel(t *testing.T) {
+	con := new(conMock)
+	con.On("Channel", nil).Return(&amqp.Channel{}, nil)
+
+	manager := ConnectionManager{
+		con:  con,
+		lock: sync.RWMutex{},
+	}
+
+	ch, err := manager.Channel()
+
+	assert.EqualValues(t, ch, &amqp.Channel{})
+	assert.NoError(t, err, "should not throw")
+
+	con.AssertExpectations(t)
 }
