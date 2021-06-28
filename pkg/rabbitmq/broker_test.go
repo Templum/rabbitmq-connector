4bd356c35e4abc6a6b48f96585c3ea0e0a57250a
Templum <templum.dev@gmail.com>
2021-06-28 17:48:01 +0000

    :white_check_mark: Added negative test for TLS

--- pkg/rabbitmq/broker_test.go
+++ pkg/rabbitmq/broker_test.go
@@ -7,6 +7,7 @@ package rabbitmq
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"testing"
 
@@ -56,10 +57,49 @@ func TestBroker_Dial(t *testing.T) {
 
 	broker := NewBroker()
 	url := fmt.Sprintf("amqp://user:pass@%s:%s/", ip, port.Port())
-	fmt.Println("Url", url)
 
 	con, err := broker.Dial(url)
 
 	assert.NotNil(t, con, "should return connection")
 	assert.NoError(t, err, "should not throw")
+}
+
+func TestBroker_DialTLS(t *testing.T) {
+	SkipIfProviderIsNotHealthy(t)
+
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "rabbitmq:3.7.4",
+		ExposedPorts: []string{"5672/tcp"},
+		WaitingFor:   wait.ForListeningPort(nat.Port("5672/tcp")),
+		Env:          map[string]string{"RABBITMQ_DEFAULT_USER": "user", "RABBITMQ_DEFAULT_PASS": "pass"},
+	}
+
+	rabbitmq, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	//nolint:golint,errcheck
+	defer rabbitmq.Terminate(ctx)
+
+	ip, err := rabbitmq.Host(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	port, err := rabbitmq.MappedPort(ctx, nat.Port("5672/tcp"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	broker := NewBroker()
+	url := fmt.Sprintf("amqps://user:pass@%s:%s/", ip, port.Port())
+
+	con, err := broker.DialTLS(url, &tls.Config{})
+
+	assert.Nil(t, con, "should not support TLS")
+	assert.Error(t, err, "should raise an error")
 }
